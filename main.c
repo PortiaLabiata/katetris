942235f6d3f9d78cfdc0673ee786cd34e2392df3
The Lost One <filippov.ia@phystech.edu>
2025-12-18 03:45:57 +0300

    Fixed UART

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include "ch.h"
-#include "chthreads.h"
 #include "hal.h"
-#include "hal_pal.h"
+#include <string.h>
 
 int main(void) {
 	halInit();
 	chSysInit();
 
-	palSetPadMode(GPIOC, 13U, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPad(GPIOC, 13U);
+	palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPad(GPIOC, GPIOC_LED);
 
+	palSetPadMode(GPIOA, 9U, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
+	palSetPadMode(GPIOA, 10U, PAL_MODE_INPUT);
+
+	const SerialConfig cfg = {
+		9600,	
+		0,
+		USART_CR2_STOP1_BITS,
+		0
+	};
+	sdStart(&SD1, &cfg);
 	while (1) {
 		palTogglePad(GPIOC, 13U);
-		chThdSleepMilliseconds(1000);
+		chThdSleepMilliseconds(100);
+		sdWrite(&SD1, (uint8_t*)"Hello, world!\r\n", strlen("Hello, world!\r\n"));
 	}
 }
