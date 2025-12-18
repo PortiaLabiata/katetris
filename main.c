67d02bcbd62d2109926fbf0659b3e6926b6b9463
The Lost One <filippov.ia@phystech.edu>
2025-12-18 18:28:33 +0300

    Made CMakeLists.txt better

--- main.c
+++ main.c
@@ -1,27 +1,15 @@
 #include "ch.h"
 #include "hal.h"
-#include <string.h>
+#include "hal_pal.h"
 
-int main(void) {
-	halInit();
+int main(int argc, char *argv[]) {
 	chSysInit();
+	halInit();
 
-	palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPad(GPIOC, GPIOC_LED);
+	palSetPadMode(GPIOC, 13, PAL_MODE_OUTPUT_PUSHPULL);
 
-	palSetPadMode(GPIOA, 9U, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
-	palSetPadMode(GPIOA, 10U, PAL_MODE_INPUT);
-
-	const SerialConfig cfg = {
-		9600,	
-		0,
-		USART_CR2_STOP1_BITS,
-		0
-	};
-	sdStart(&SD1, &cfg);
 	while (1) {
-		palTogglePad(GPIOC, 13U);
-		chThdSleepMilliseconds(100);
-		sdWrite(&SD1, (uint8_t*)"Hello, world!\r\n", strlen("Hello, world!\r\n"));
+		palTogglePad(GPIOC, 13);
+		chThdSleepMilliseconds(1000);
 	}
 }
