66702f7b68c18f7d039fcd2d3b49c459e4b9e06a
The Lost One <filippov.ia@phystech.edu>
2025-12-19 00:10:42 +0300

    Fixed some stuff here and there

--- main.c
+++ main.c
@@ -1,23 +1,60 @@
 #include "ch.h"
-#include "chthreads.h"
 #include "hal.h"
-#include "hal_pal.h"
+#include "chprintf.h"
 
-static THD_WORKING_AREA(wa_serial, 128);
-static THD_FUNCTION(thd_serial, arg) {
+#define GPIO_LED 13
+int flag = 1;
+
+void cmd_on(BaseSequentialStream *s) {
+	flag = 0;
+	chprintf(s, "led on ok");
+	palSetPad(GPIOC, GPIO_LED);
+}
+
+void cmd_off(BaseSequentialStream *s) {
+	flag = 0;
+	chprintf(s, "led off ok");
+	palClearPad(GPIOC, GPIO_LED);
+}
+
+void cmd_blink(BaseSequentialStream *s) {
+	flag = 1;
+	chprintf(s, "led blink ok");
+}
+
+#define streq(a, b) (strcmp(a, b) == 0)
+static THD_WORKING_AREA(wa_shell, 512);
+static THD_FUNCTION(thd_shell, arg) {
 	while (1) {
-		sdWrite(&SD1, (uint8_t*)"Hello, world!\n", 14);
-		chThdSleepMilliseconds(1000);
+		char c = sdGet(&SD1);
+
+		BaseSequentialStream *s = (BaseSequentialStream*)&SD1;
+		switch (c) {
+			case '1':
+				cmd_on(s);
+				break;
+			case '0':
+				cmd_off(s);
+				break;
+			case 'b':
+				cmd_blink(s);
+				break;
+			default:
+				break;
+		}
 	}
 }
 
 static THD_WORKING_AREA(wa_blink, 128);
 static THD_FUNCTION(thd_blink, arg) {
 	while (1) {
-		palTogglePad(GPIOC, 13);
-		chThdSleepMilliseconds(500);
+		while (!flag)
+			chThdSleepMilliseconds(1);
+		palTogglePad(GPIOC, GPIO_LED);
+		chThdSleepMilliseconds(499);
 	}
 }
+
 
 int main(int argc, char *argv[]) {
 	chSysInit();
@@ -29,14 +66,18 @@ int main(int argc, char *argv[]) {
 	palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);
 
 	sdStart(&SD1, NULL);
-	chThdSleepMilliseconds(1);
-	thread_t *thread_serial = chThdCreateStatic(wa_serial, 
-					sizeof(wa_serial),
-					NORMALPRIO,
-					thd_serial, NULL);
 	chThdCreateStatic(wa_blink, 
-					sizeof(wa_blink),
-					NORMALPRIO,
-					thd_blink, NULL);
-	chThdWait(thread_serial);
+				sizeof(wa_blink),
+				NORMALPRIO,
+				thd_blink, NULL);
+	thread_t *shthd = chThdCreateStatic(wa_shell,
+				sizeof(wa_shell),
+				NORMALPRIO,
+				thd_shell, NULL);
+	chThdWait(shthd);
+}
+
+void HardFault_Handler(void) {
+	while (1)
+		;
 }
