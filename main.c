34e7def61aa89e248499b6118a4e6f1896ea6083
The Lost One <filippov.ia@phystech.edu>
2025-12-18 18:54:36 +0300

    Non-working version

--- main.c
+++ main.c
@@ -2,12 +2,28 @@
 #include "hal.h"
 #include "hal_pal.h"
 
+static THD_WORKING_AREA(wa_serial, 128);
+static THD_FUNCTION(thd_serial, arg) {
+	while (1) {
+		sdWrite(&SD1, (uint8_t*)"Hello, world!\n", 14);
+		chThdSleepMilliseconds(1000);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	chSysInit();
 	halInit();
 
 	palSetPadMode(GPIOC, 13, PAL_MODE_OUTPUT_PUSHPULL);
 
+	palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
+	palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);
+
+	sdStart(&SD1, NULL);
+	(void)chThdCreateStatic(wa_serial, 
+					THD_WORKING_AREA_SIZE(wa_serial),
+					NORMALPRIO,
+					thd_serial, NULL);
 	while (1) {
 		palTogglePad(GPIOC, 13);
 		chThdSleepMilliseconds(1000);
